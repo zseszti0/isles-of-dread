66d0c739bc1dc1ca88f583e61f7a322aa5c8b41c
zseszti0 <zseszti42.3@gmail.com>
2022-10-17 10:07:08 +0200

    hetfo infp

--- rpg/script.js
+++ rpg/script.js
@@ -3,15 +3,22 @@ let info = document.getElementById("info")
 let accountlogin = document.getElementById("accountlogin")
 let accountsignup = document.getElementById("accountsignup")
 
+let signtext = document.getElementById("signText")
+let logtext = document.getElementById("logText")
+
 function signUp(){
     mainMenu.style.display = "none"
     info.style.display = "block"
     accountlogin.style.display = "none"
     accountsignup.style.display = "block"
+    logtext.style.display = "none"
+    signtext.style.display = "block"
 
 }
 
 function logIn(){
+    logtext.style.display = "block"
+    signtext.style.display = "none"
     mainMenu.style.display = "none"
     info.style.display = "block"
     accountlogin.style.display = "block"
